f73e7f107c7b2b063b46a94f3a5e9ed3c90da1ad
Faiz Faiyaz <forcourse23@gmail.com>
2025-02-25 21:24:18 +0530

    Initial commit

---
+++ src/main/java/com/scoreboard/realtime_scoreboard/RealtimeScoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.scoreboard.realtime_scoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeScoreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoreboard/realtime_scoreboard/RealtimeScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoreboard.realtime_scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
